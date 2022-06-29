f5e472216040b4a452ec24568a7e5c6ec7f0001a
skoczy01 <skoczy01@wp.pl>
2022-06-29 13:46:12 +0200

    Style new section and bug fixed

--- src/components/Card/CreatorCard.js
+++ src/components/Card/CreatorCard.js
@@ -1,16 +1,28 @@
 import classes from "./CreatorCard.module.scss";
+import eth from "../../images/Eth.png";
 
 export const CreatorCard = (props) => {
   return (
     <div className={classes.card}>
       <div className={classes["data-box"]}>
-        <img src={props.img} alt="" />
+        <div className={classes["main-bgc"]}>
+          <img src={props.img} alt="" />
+        </div>
         <div className={classes.data}>
           <h3>{props.name}</h3>
-          <h4>{props.totalVolume}</h4>
+          <div className={classes["value-box"]}>
+            <div className={classes.bgc}>
+              <img src={eth} alt="" />{" "}
+            </div>
+            <p>{props.totalVolume}</p>
+          </div>
         </div>
       </div>
-      <div className={classes.dots}>...</div>
+      <div className={classes.dots}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
--- src/components/TopCreators/CreatorsContent.js
+++ src/components/TopCreators/CreatorsContent.js
@@ -1,11 +1,26 @@
 import { CreatorCard } from "../Card/CreatorCard";
-import img1 from "../../images/creator1.png";
+import classes from "./CreatorsContent.module.scss";
 
+import img1 from "../../images/creator1.png";
+import img2 from "../../images/creator2.png";
+import img3 from "../../images/creator3.png";
+import img4 from "../../images/creator4.png";
+import img5 from "../../images/creator5.png";
+import img6 from "../../images/creator6.png";
+import img7 from "../../images/creator7.png";
+import img8 from "../../images/creator8.png";
+import img9 from "../../images/creator9.png";
 export const CreatorsContent = () => {
   const data = [
     { id: 0, name: "Rick Storms", totalVolume: "19,400,50 ETH", img: img1 },
-    // { id: 0, name: "Sam Son", totalVolume: "6,400,50 ETH", img: "" },
-    // { id: 0, name: "Jake robert", totalVolume: "8,400,50 ETH", img: "" },
+    { id: 1, name: "Sam Son", totalVolume: "6,400,50 ETH", img: img2 },
+    { id: 2, name: "Jake robert", totalVolume: "8,400,50 ETH", img: img3 },
+    { id: 3, name: "Jake robert", totalVolume: "8,400,50 ETH", img: img4 },
+    { id: 4, name: "Jake robert", totalVolume: "8,400,50 ETH", img: img5 },
+    { id: 5, name: "Jake robert", totalVolume: "8,400,50 ETH", img: img6 },
+    { id: 6, name: "Jake robert", totalVolume: "8,400,50 ETH", img: img7 },
+    { id: 7, name: "Jake robert", totalVolume: "8,400,50 ETH", img: img8 },
+    { id: 8, name: "Jake robert", totalVolume: "8,400,50 ETH", img: img9 },
   ];
 
   const items = data.map((item) => {
@@ -18,5 +33,9 @@ export const CreatorsContent = () => {
       />
     );
   });
-  return <div className="container">{items}</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes["grid-wrapper"]}>{items}</div>
+    </div>
+  );
 };
